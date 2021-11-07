ca1cf329f96d3e5e3c6aa45dc7f7840553ef3dba
GBAleksandrGB <tomilov.alex@gmail.com>
2021-11-07 15:47:58 +0300

    fixed an error

--- task_7_2.py
+++ task_7_2.py
@@ -11,11 +11,21 @@ with webdriver.Chrome() as driver:
     products = driver.find_elements(By.XPATH, '//mvid-product-mini-card')
     for product in products:
         product_list = product.text.split('\n')
-        product_for_db = {'Товар': product_list[1],
-                          'Скидка': product_list[0],
-                          'Рейтинг': product_list[2],
-                          'Отзывы': product_list[3],
-                          'Новая цена': product_list[4].replace(' ', ''),
-                          'Старая цена': product_list[5].replace(' ', '')
-                          }
+        if len(product_list) > 5:
+            product_for_db = {'Товар': product_list[1],
+                              'Скидка': product_list[0],
+                              'Рейтинг': product_list[2],
+                              'Отзывы': product_list[3],
+                              'Новая цена': product_list[4].replace(' ', ''),
+                              'Старая цена': product_list[5].replace(' ', '')
+                              }
+        else:
+            product_for_db = {'Товар': product_list[1],
+                              'Скидка': product_list[0],
+                              'Рейтинг': 'отсутствует',
+                              'Отзывы': 'отсутствуют',
+                              'Новая цена': product_list[3].replace(' ', ''),
+                              'Старая цена': product_list[4].replace(' ', '')
+                              }
         collection.insert_one(product_for_db)
+        # print(product_for_db)
